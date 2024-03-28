1f19bc92458ae301244d64320b12ae2b69a98c31
Manfred Dimala <galileum5@gmail.com>
2024-03-28 07:35:37 +0400

    Add Input to Component

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HousingLocation} from '../housinglocation';
+
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
 export class HousingLocationComponent {
 
+  @Input() housingLocation!: HousingLocation;/*The exclamation point
+   is called the non-null assertion operator and it tells the 
+   TypeScript compiler that the value of 
+   this property won't be null or undefined
+*/
 }
